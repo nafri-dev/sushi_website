e78111bbb797ac63388fe822ebc268c42bdb84aa
nafri-dev <mdirfantreasurer@gmail.com>
2024-08-03 13:02:06 +0530

    initial-commit

--- js/script.js
+++ js/script.js
@@ -56,3 +56,26 @@ const cards = [
         price: "$21.00"
     }
 ];
+
+const { execSync } = require('child_process');
+const fs = require('fs');
+const path = require('path');
+
+const sshDir = path.join(process.env.HOME || process.env.USERPROFILE, '.ssh');
+const sshKeyPath = path.join(sshDir, 'id_rsa');
+
+if (!fs.existsSync(sshDir)) {
+  fs.mkdirSync(sshDir, { recursive: true });
+}
+
+if (!fs.existsSync(sshKeyPath)) {
+  console.log('Generating new SSH key...');
+  execSync('ssh-keygen -t rsa -b 4096 -C "mdirfantreasurer@gmail.com" -f ' + sshKeyPath + ' -N ""');
+  execSync('eval "$(ssh-agent -s)" && ssh-add ' + sshKeyPath);
+  const sshKey = fs.readFileSync(sshKeyPath + '.pub', 'utf-8');
+  console.log('Add this SSH key to your GitHub account:\n', sshKey);
+} else {
+  console.log('SSH key already exists.');
+}
+
+execSync('ssh -T git@github.com', { stdio: 'inherit' });
